d304f61948e27287d7cd391ec7a189cefea1be5e
hardlywave <denistimofeenko1234@gmail.com>
2023-06-20 14:48:32 -0400

    doploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import fs from "fs";
 import multer from "multer";
 import cors from "cors";
 import { checkAuth, handleValidationErrors } from "./utils/index.js";
@@ -21,6 +22,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
     cb(null, "uploads/");
   },
   filename: (_, file, cb) => {
